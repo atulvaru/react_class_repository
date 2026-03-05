b14e737992f47a94573068a9301abd9cbf751c0e
atulvaru <atulk2512001@gmail.com>
2026-03-05 17:47:42 +0530

    conditional styling in react

--- src/components/ServiceCard.jsx
+++ src/components/ServiceCard.jsx
@@ -1,6 +1,11 @@
 import "./ServiceCard.css";
 export const ServiceCard = (props) => {
   const { name, img, rating, summary, genre, imdb } = props.data;
+
+  const ratingClass = rating >= 8.5 ? "color_green" : "color_red";
+ const btnClass = Number(rating) >= 8.5 ? "btn_green" : "btn_red";
+
+
   return (
     <div className="movie-card">
       {/* 🖼 Movie Poster */}
@@ -9,14 +14,17 @@ export const ServiceCard = (props) => {
       <div className="movie-content">
         {/* 📌 Dynamic Values in JSX */}
         <h2 className="movie-title">{name}</h2>
-        <p className="movie-rating">⭐ {rating}</p>
+        <p className="movie-rating">
+          ⭐ Rating{" "}
+          {<span className={`rating ${ratingClass}`}> {rating} </span>}
+        </p>
         <p className="movie-genre">{genre}</p>
         <p className="movie-summary">{summary}</p>
 
         {/* 🔐 Conditional Rendering */}
         <a href={imdb} target="_blank">
-      <button className="movie-btn">Watch Now</button>
-    </a>
+          <button className={`movie-btn ${btnClass}`}>Watch Now</button>
+        </a>
       </div>
     </div>
   );
