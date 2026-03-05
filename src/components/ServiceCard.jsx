6d78b103a82c1b6173b6882f654b57d8f3c6000e
atulvaru <atulk2512001@gmail.com>
2026-03-06 01:02:21 +0530

    adding module base css like Service.modul.css

--- src/components/ServiceCard.jsx
+++ src/components/ServiceCard.jsx
@@ -1,29 +1,29 @@
-import "./ServiceCard.css";
+// import "./ServiceCard.css";
+import styles from "./ServiceCard.module.css";
 export const ServiceCard = (props) => {
   const { name, img, rating, summary, genre, imdb } = props.data;
 
-  const ratingClass = rating >= 8.5 ? "color_green" : "color_red";
- const btnClass = Number(rating) >= 8.5 ? "btn_green" : "btn_red";
-
+  const ratingClass = rating >= 8.5 ? styles.color_green : styles.color_red;
+  const btnClass = Number(rating) >= 8.5 ? styles.btn_green : styles.btn_red;
 
   return (
-    <div className="movie-card">
+    <div className={styles["movie-card"]}>
       {/* 🖼 Movie Poster */}
       <img src={img} alt="movie-poster" />
 
-      <div className="movie-content">
+      <div className={styles["movie-content"]}>
         {/* 📌 Dynamic Values in JSX */}
-        <h2 className="movie-title">{name}</h2>
-        <p className="movie-rating">
+        <h2 className={styles["movie-title"]}>{name}</h2>
+        <p className={styles["movie-rating"]}>
           ⭐ Rating{" "}
-          {<span className={`rating ${ratingClass}`}> {rating} </span>}
+          {<span className={`${styles.rating} ${ratingClass}`}> {rating} </span>}
         </p>
-        <p className="movie-genre">{genre}</p>
-        <p className="movie-summary">{summary}</p>
+        <p className={styles["movie-genre"]}>{genre}</p>
+        <p className={styles["movie-summary"]}>{summary}</p>
 
         {/* 🔐 Conditional Rendering */}
         <a href={imdb} target="_blank">
-          <button className={`movie-btn ${btnClass}`}>Watch Now</button>
+          <button className={`${styles["movie-btn"]} ${btnClass}`}>Watch Now</button>
         </a>
       </div>
     </div>
