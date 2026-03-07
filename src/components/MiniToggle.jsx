1b897262b973fa74536ada81adc9bccc813c6da4
atulvaru <atulk2512001@gmail.com>
2026-03-08 00:39:40 +0530

    Mini project toggle on off

--- src/components/MiniToggle.jsx
+++ src/components/MiniToggle.jsx
@@ -7,17 +7,21 @@ export const MiniToggle = () => {
     <>
       <div
         className={`w-[6rem] h-[3rem] mx-auto rounded-full flex items-center cursor-pointer border-2 border-gray-400 px-1 transition-colors duration-300 
-            ${isOn ? "bg-green-500" : "bg-gray-300"}`}
+        ${isOn ? "bg-green-500" : "bg-gray-300"}`}
         onClick={() => setIsOn(!isOn)}
       >
         <div
-          className={`w-[2.5rem] h-[2.5rem] bg-white rounded-full flex items-center border-2 border-gray-400 justify-center text-black font-semibold transform transition-transform duration-300 ${
+          className={`w-[2.5rem] h-[2.5rem] bg-white rounded-full flex items-center border-2 border-gray-400 justify-center text-xl transform transition-transform duration-300 ${
             isOn ? "translate-x-12" : "translate-x-0"
           }`}
         >
-          {isOn ? "ON" : "OFF"}
+          {isOn ? "😎" : "😴"}
         </div>
       </div>
+
+      <p className="text-center mt-4 text-lg font-semibold">
+        {isOn ? "⚡ Power ON 🔋" : "🌙 Power OFF 💤"}
+      </p>
     </>
   );
 };
